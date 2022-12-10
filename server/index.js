5a549842652855e37d39c7e3ab9e5a3329c0b197
evanmalherbe <evanmalherbe@gmail.com>
2022-12-10 14:23:54 +0200

    Updated some dependencies.

--- server/index.js
+++ server/index.js
@@ -12,20 +12,24 @@ const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 3001;
 
 // Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
+if (!isDev && cluster.isMaster)
+{
   console.error(`Node cluster master ${process.pid} is running`);
 
   // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < numCPUs; i++)
+  {
     cluster.fork();
   }
 
-  cluster.on("exit", (worker, code, signal) => {
+  cluster.on("exit", (worker, code, signal) =>
+  {
     console.error(
       `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
     );
   });
-} else {
+} else
+{
   const app = express();
 
   /* Use Helmet middleware to improve security
@@ -115,38 +119,44 @@ https://mongoosejs.com/docs/connections.html */
   // Initial connection to db and error handling if initial connection fails
   mongoose
     .connect(
-      process.env.MONGODB_URI ||
-        `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
+      `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
     )
     .catch((error) =>
       console.log("Failed initial connection to db. Error is: " + error)
     );
 
+  // process.env.MONGODB_URI ||
+
   // Message if success in connecting to db
-  mongoose.connection.once("open", function () {
+  mongoose.connection.once("open", function ()
+  {
     console.log("Successfully connected to the database");
   });
 
   // Error handling if connection to db fails after an initially successful connection
-  mongoose.connection.on("error", (error) => {
-    if (error) {
+  mongoose.connection.on("error", (error) =>
+  {
+    if (error)
+    {
       console.log("An error occurred after initial connection to db: " + error);
-    } else {
+    } else
+    {
       console.log("Connection Established");
     }
   });
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get("*", function (request, response) {
+  app.get("*", function (request, response)
+  {
     response.sendFile(
       path.resolve(__dirname, "../react-ui/build", "index.html")
     );
   });
 
-  app.listen(PORT, function () {
+  app.listen(PORT, function ()
+  {
     console.error(
-      `Node ${
-        isDev ? "dev server" : "cluster worker " + process.pid
+      `Node ${isDev ? "dev server" : "cluster worker " + process.pid
       }: listening on port ${PORT}`
     );
   });
